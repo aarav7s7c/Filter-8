fc77eeab54da3db63e4fb9a7619d7b1a87987a66
Aarav <94453317+aarav7s7c@users.noreply.github.com>
2024-06-01 17:36:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+nose_x = 0;
+nose_y = 0;
+
+function rhot() {
+  console.log("Working");
+}
+function preload() {
+  musta = loadImage(
+    "https://i.postimg.cc/fypvbHpz/Moustache-Transparent-Free-PNG.png"
+  );
+  lip = loadImage("https://i.postimg.cc/7LB42YX7/lips-PNG6217.png");
+}
+
 function setup() {
   canvas = createCanvas(650, 450);
   canvas.position(350, 250);
@@ -6,26 +19,28 @@ function setup() {
   tint_color = "";
 
   poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses)
+  poseNet.on("pose", gotPoses);
 }
 
 function modelLoaded() {
-  console.log("Posenet is initialised")
+  console.log("Posenet is initialised");
 }
 
 function gotPoses(results) {
   if (results.length > 0) {
-    console.log(results)
-    console.log("nose x = " + results[0].pose.nose.x);
-    console.log("nose y = " + results[0].pose.nose.y);
+    console.log(results);
+    nose_x = results[0].pose.nose.x;
+    nose_y = results[0].pose.nose.y;
+    console.log("nose x = " + nose_x);
+    console.log("nose y = " + nose_y);
   }
 }
 
 function draw() {
   image(video, 0, 0, 650, 450);
-   cursor("default");
+  cursor("default");
+  image(musta, nose_x - 50, nose_y - 25, 120, 50);
 }
-
 function TKshot() {
   save("good u");
 }
